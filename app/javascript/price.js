0570c53aa42e087200123edd6975940a377da5d0
maaaaqqqq <marino2636@gmail.com>
2022-01-20 07:50:24 +0900

    商品出品ページの実装

---
+++ app/javascript/price.js
@@ -0,0 +1,12 @@
+window.addEventListener('load',() => {
+  const priceInput = document.getElementById('item-price');
+  priceInput.addEventListener('input',() => {
+    const inputValue = priceInput.value;
+    const addTaxDom = document.getElementById("add-tax-price");
+    const calcTax = Math.floor(inputValue * 0.1 );
+    addTaxDom.innerHTML = calcTax;
+    const makeProfitDom = document.getElementById("profit");
+    makeProfitDom.innerHTML = Math.floor(inputValue - calcTax);
+  });
+});
+
